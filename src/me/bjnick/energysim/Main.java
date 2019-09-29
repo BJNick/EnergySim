4a197b78250528f7c0ede379e6d5d1000fdbd770
BJNick <>
2019-09-29 12:32:00 -0600

    Init commit

---
+++ src/me/bjnick/energysim/Main.java
@@ -0,0 +1,10 @@
+package me.bjnick.energysim;
+
+public class Main {
+
+    public static void main(String[] args) {
+        ViewFrame frame = new ViewFrame();
+        frame.setVisible(true);
+    }
+
+}
---
+++ src/me/bjnick/energysim/ViewFrame.java
@@ -0,0 +1,16 @@
+package me.bjnick.energysim;
+
+import javax.swing.*;
+
+class ViewFrame extends JFrame {
+
+        ViewFrame() {
+
+            super();
+
+            setSize(600, 400);
+
+
+        }
+
+}
